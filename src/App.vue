2adf1b080461e41800dbf48e93713f91c7c507fe
Nabil-nl <nabilouhmida9@gmail.com>
2024-07-10 15:42:16 +0100

    add

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
     <div id="app">
       <HelloWorld />
       <Section />
+      <Right />
     </div>
   </template>
   
   <script>
   import HelloWorld from './components/HelloWorld.vue';
   import Section from './components/Section.vue';
+  import Right from './components/Right.vue';
   
   export default {
     name: 'App',
     components: {
       HelloWorld,
-      Section 
+      Section,
+      Right
     }
   };
   </script>
